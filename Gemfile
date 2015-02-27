c2dfcc284f7a7aa80f7c7ec2f4daaea0a7b0e388
Sonja R. Maddox <[sonjarmaddox@yahoo.com]>
2015-02-26 20:51:43 -0600

    changed colors

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'thin'
 #adding font awesomeness
 gem 'font-awesome-rails'
 
+
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,20 @@
 class HomeController < ApplicationController
   def index
-     right_now= Time.now.strftime("%a, %b %d %Y %I:%M %p") 
-    @current_time = right_now
-    @greet_message = case right_now 
-    when 5..11
-      "Good Morning!"
-    when 12..15
-      "Good Afternoon!"
-    when 16..19
-      "Good Evening!"
-    when 20..23, 0..4
-      "Good Night!"
-    end
+     right_now= Time.new 
+    @current_time = right_now.to_formatted_s(:long)
+    @greet_message = case right_now.hour 
+      when 1..11
+       "Good Morning!" 
+        
+      when 12..15
+       "Good Afternoon!"
+          
+      when 16..19
+       "Good Evening!"
+    
+      when 20..24
+       "Good Night!"
+    end  
   end
 end
 

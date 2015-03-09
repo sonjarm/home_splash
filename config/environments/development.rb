c7bb0dc1ee6080114acd48ca672f85dff6dd3391
Sonja R. Maddox <[sonjarmaddox@yahoo.com]>
2015-03-09 09:19:09 -0500

    added letter opener gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,10 @@ Rails.application.configure do
 
   #Link for emails etc.
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  
+ 
+  #use letter opener gem
+  config.action_mailer.delivery_method = :letter_opener
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
